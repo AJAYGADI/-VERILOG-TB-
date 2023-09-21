4caf928413de30d22f78e727f97eef279a9f1740
AJAYGADI <94074945+AJAYGADI@users.noreply.github.com>
2023-09-21 21:12:41 +0530

    twisted ringcounter_tb.v
    This testbench (tb_twisted_ring_counter) includes clock generation, instantiation of the twisted ring counter module (twisted_ring_counter), and a test scenario.
    
    The clk signal is generated by toggling it every time step with a delay of 5 time units (half a clock cycle).
    
    The initial block starts the test scenario:
    
    Inputs are initialized (clk and reset).
    The $display system task is used to monitor and display the counter values at each time step.
    The reset signal is asserted and held for 10 clock cycles to clear the counter.
    The counter is shifted for 16 clock cycles, with the counter values displayed at each step.
    The simulation is ended using the $finish system task.

---
+++ twisted ringcounter_tb.v
@@ -0,0 +1,44 @@
+module tb_twisted_ring_counter;
+
+  reg clk;                 // Clock signal
+  reg reset;               // Reset signal (active low)
+  wire [3:0] count;        // 4-bit counter output
+
+  // Instantiate the twisted ring counter
+  twisted_ring_counter uut (
+    .clk(clk),
+    .reset(reset),
+    .count(count)
+  );
+
+  // Clock generation
+  always begin
+    clk = ~clk;            // Toggle the clock every time step
+    #5;                   // Delay for half a clock cycle
+  end
+
+  // Test scenario
+  initial begin
+    // Initialize inputs
+    clk = 0;
+    reset = 0;
+
+    // Monitor and display counter values
+    $display("Time=%t, Count=%b", $time, count);
+
+    // Apply reset
+    reset = 1;
+    #10;                  // Hold reset for 10 clock cycles
+    reset = 0;
+
+    // Shift the counter for 16 clock cycles
+    repeat (16) begin
+      #10;                // Shift the counter every 10 clock cycles
+      $display("Time=%t, Count=%b", $time, count);
+    end
+
+    // End simulation
+    $finish;
+  end
+
+endmodule
