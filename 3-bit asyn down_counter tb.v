3329689e443a8587725c5492beff471317d55a9d
AJAYGADI <94074945+AJAYGADI@users.noreply.github.com>
2023-09-21 16:12:33 +0530

    3-bit asyn down_counter tb.v
    The testbench module tb_async_down_counter contains registers for clock (clk), reset (rst), and a wire for the counter output (count).
    
    It instantiates the async_down_counter module (uut) to test its functionality.
    
    The always block generates the clock signal by toggling it every time step and introducing a delay of 5 time units (half a clock cycle).
    
    Inside the initial block, the test scenario is defined:
    
    Inputs are initialized.
    The reset signal is asserted and held for 10 clock cycles to clear the counter.
    The $display system task is used to monitor and display the counter values at each time step.
    The simulation is allowed to run for an additional 30 clock cycles to observe counter behavior.
    Finally, the simulation is ended using $finish.

---
+++ 3-bit asyn down_counter tb.v
@@ -0,0 +1,41 @@
+module tb_async_down_counter;
+
+  reg clk;               // Clock signal
+  reg rst;               // Reset signal (active low)
+  wire [2:0] count;      // Counter output
+
+  // Instantiate the asynchronous down counter
+  async_down_counter uut (
+    .clk(clk),
+    .rst(rst),
+    .count(count)
+  );
+
+  // Clock generation
+  always begin
+    clk = ~clk;          // Toggle the clock every time step
+    #5;                 // Delay for half a clock cycle
+  end
+
+  // Test scenario
+  initial begin
+    // Initialize inputs
+    clk = 0;
+    rst = 0;
+
+    // Reset the counter
+    rst = 1;
+    #10;                // Hold reset for 10 clock cycles
+    rst = 0;
+
+    // Monitor and display counter values
+    $display("Time=%t, Count=%b", $time, count);
+
+    // Count for a few clock cycles
+    #30;
+
+    // End simulation
+    $finish;
+  end
+
+endmodule
